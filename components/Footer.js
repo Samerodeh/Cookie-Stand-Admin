252800fc09db46bae21007ade1f54170dc76bdd3
Samer Odeh <samerodeh220@gmail.com>
2021-09-13 19:20:04 +0300

    solve lab 37

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+function Footer() {
+    return (
+        <footer className="p-5 text-left bg-green-500 h-15">&copy;2021</footer>
+    )
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="p-5 text-3xl text-left bg-green-500 font h-15">
+            <h1>
+                Cookie Stand Admin
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
